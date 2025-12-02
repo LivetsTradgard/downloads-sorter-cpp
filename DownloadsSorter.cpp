bb1513685b29a2e81e7129344a49cb43a79f9e97
Nik Lisicyn <Nik.lisicyn2014@gmail.com>
2025-12-02 20:04:11 +0300

    Initial commit

---
+++ DownloadsSorter.cpp
@@ -0,0 +1,30 @@
+﻿#include <filesystem>
+#include <iostream>
+#include <string>
+
+
+namespace fs = std::filesystem;
+
+int main() {
+
+	std::string path;
+	std::cout << "Enter path to downloads folder: ";
+	std::getline(std::cin, path);
+
+	fs::path downloads = path;
+
+	if (!fs::exists(downloads)) {
+		std::cout << "This folder doesn't exist.\n";
+	}
+	
+	if (!fs::is_directory(downloads)) {
+		std::cout << "This isn't a directory.\n";
+	}
+
+	for (const auto& entry : fs::directory_iterator(downloads)) {
+		if (entry.is_regular_file()) {
+			std::cout << entry.path().extension().string() << "\n";
+		}
+	}
+	return 0;
+}
